d9593a956118b7611cd12ef6f43a5ccb705db207
4bstractor <theunderstander@gmail.com>
2013-05-17 18:40:15 -0400

    Replaced two bits of migration code with functionally identical pieces due to issues with postgres

--- db/migrate/20121115094430_increate_trace_colunm_limit.rb
+++ db/migrate/20121115094430_increate_trace_colunm_limit.rb
@@ -1,6 +1,6 @@
 class IncreateTraceColunmLimit < ActiveRecord::Migration
   def up
-    change_column :builds, :trace, :text, :limit => 4294967295
+    change_column :builds, :trace, :text
   end
 
   def down
--- db/migrate/20130114153451_change_schedule_invertal.rb
+++ db/migrate/20130114153451_change_schedule_invertal.rb
@@ -1,9 +1,12 @@
 class ChangeScheduleInvertal < ActiveRecord::Migration
   def up
-    change_column :projects, :polling_interval, :integer, null: true
+    # Gets around postgres issue with type casting
+    remove_column :projects, :polling_interval
+    add_column :projects, :polling_interval, :integer, null: true
   end
 
   def down
-    change_column :projects, :polling_interval, :string, null: true
+    remove_column :projects, :polling_interval
+    add_column :projects, :polling_interval, :string, null: true
   end
 end
